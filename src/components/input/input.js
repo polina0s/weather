8855258f2bf42e138b44542317d92a9fe2931376
polina0s <polina.osintseva000@gmail.com>
2023-04-13 17:11:53 +0400

    add modal class and methods

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,6 +1,11 @@
 class Input {
   constructor() {
     this.cityInput = document.querySelector(".weather_locationInput-input");
+    this.modal = document.querySelector(".modal");
+
+    this.cityInput.addEventListener("input", () => {
+      this.modal.classList.add("modal--active");
+    });
   }
 }
 
---
+++ src/components/modal/modal.js
@@ -0,0 +1,13 @@
+class Modal {
+  constructor() {
+    this.modalWind = document.querySelector(".modal");
+  }
+
+  addActiveClass() {
+    this.modalWind.classList.add("modal--active");
+  }
+
+  removeActiveClass() {
+    this.modalWind.classList.remove("modal--active");
+  }
+}
