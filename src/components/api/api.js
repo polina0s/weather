742bb81881f5180d3fd1c8cd5601bbd0e5d3f335
polina0s <polina.osintseva000@gmail.com>
2023-04-19 18:36:19 +0400

    improved getCities method for api class, add method getCountries

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -5,18 +5,29 @@ class Api {
   }
 
   getCities(name) {
-    let countries = [];
     let cities = [];
     fetch(`${this.geoUrl}search?name=${name}`)
       .then((response) => response.json())
       .then((result) => {
-        let a = result.results;
-        a.forEach((el) => {
-          countries.push(el.country);
+        let results = result.results;
+        results.forEach((el) => {
           cities.push(el.name);
         });
       });
-    return countries, cities;
+    return cities;
+  }
+
+  getCountries(name) {
+    let countries = [];
+    fetch(`${this.geoUrl}search?name=${name}`)
+      .then((response) => response.json())
+      .then((result) => {
+        let results = results.results;
+        results.forEach((el) => {
+          countries.push(el.country);
+        });
+      });
+    return countries;
   }
 }
 
