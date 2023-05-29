0036ff9668b9cd7177ce1b5b025f1578b6c40e41
polina0s <polina.osintseva000@gmail.com>
2023-05-29 18:01:37 +0400

    fixed layout for weekWeather el

--- src/components/weatherCont/weatherCont.js
+++ src/components/weatherCont/weatherCont.js
@@ -1,5 +1,6 @@
 import { api } from "../api/api";
 import { CurrentWeather } from "../currentWeather/currentWeather";
+import { WeekWeather } from "../weekWeather/weekWeather";
 
 export class WeatherCont {
   constructor() {
@@ -20,18 +21,35 @@ export class WeatherCont {
       this.appendCurrentWeather(this.currentWeather.element);
     });
 
-    // this.api.getForecast(latitude, longitude).then((forecast) => {
-    //   let temp = forecast.temperature_2m;
-    //   let timeArr = forecast.time;
-    //   let date = [];
+    this.api.getForecast(latitude, longitude).then((forecast) => {
+      let temp = forecast.temperature_2m;
+      let timeArr = forecast.time;
+      let dates = [];
 
-    //   for (let i = 0; i < timeArr.length; i++) {
-    //     let y = timeArr[i].substring(5, 10).replaceAll("-", ".");
-    //     date.push(y);
-    //   }
+      for (let i = 0; i < timeArr.length; i++) {
+        let date = timeArr[i].substring(5, 10).replaceAll("-", ".");
+        dates.push(date);
+      }
+      // time = dates,
+      // temperature = temp,
 
-    //   console.log(date);
-    // });
+      this.weekWeather = new WeekWeather(dates, temp);
+
+      console.log(dates, temp);
+      console.log(dates[167]);
+
+      this.appendWeekeather(this.weekWeather.element);
+
+      // let timeArr = forecast.time;
+      // let dates = [];
+
+      // for (let i = 0; i < timeArr.length; i++) {
+      //   let date = timeArr[i].substring(5, 10).replaceAll("-", ".");
+      //   dates.push(date);
+      // }
+
+      // console.log(dates);
+    });
   }
 
   clearWeatherCont() {
@@ -41,5 +59,9 @@ export class WeatherCont {
   appendCurrentWeather(element) {
     this.weatherCont.append(element);
     this.currentWeather.endTransition();
+  }
+
+  appendWeekeather(element) {
+    this.weatherCont.append(element);
   }
 }
--- src/components/weekWeather/weekWeather.js
+++ src/components/weekWeather/weekWeather.js
@@ -1,37 +1,34 @@
-class WeekWeather {
-  constructor(data) {
-    this.createWeekWeatherElement({
-      day: data.day,
-      temperature: data.temperature,
-    });
+export class WeekWeather {
+  constructor(time, temp) {
+    this.createWeekWeatherElement(time, temp);
   }
 
-  createWeekWeatherElement({ time, temp }) {
+  createWeekWeatherElement(time, temp) {
     this.element = document.createElement("div");
     this.element.classList.add("weather_week");
 
     this.element.innerHTML = `
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[24]} </div>
-      <div class="weather_week--temp"> ${temp[24]} </div>
+      <div class="weather_week--day"> ${time[23]} </div>
+      <div class="weather_week--temp"> ${temp[23]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[48]} </div>
-      <div class="weather_week--temp"> ${temp[48]} </div>
+      <div class="weather_week--day"> ${time[47]} </div>
+      <div class="weather_week--temp"> ${temp[47]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[72]} </div>
-      <div class="weather_week--temp"> ${temp[72]} </div>
+      <div class="weather_week--day"> ${time[71]} </div>
+      <div class="weather_week--temp"> ${temp[71]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[96]} </div>
-      <div class="weather_week--temp"> ${temp[96]} </div>
+      <div class="weather_week--day"> ${time[95]} </div>
+      <div class="weather_week--temp"> ${temp[95]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${120} </div>
-      <div class="weather_week--temp"> ${temp[120]} </div>
+      <div class="weather_week--day"> ${time[119]} </div>
+      <div class="weather_week--temp"> ${temp[119]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[144]} </div>
-      <div class="weather_week--temp"> ${temp[144]} </div>
+      <div class="weather_week--day"> ${time[143]} </div>
+      <div class="weather_week--temp"> ${temp[143]} </div>
     <div class="weather_week--el">
-      <div class="weather_week--day"> ${time[168]} </div>
-      <div class="weather_week--temp"> ${temp[168]} </div>            
+      <div class="weather_week--day"> ${time[167]} </div>
+      <div class="weather_week--temp"> ${temp[167]} </div>            
     `;
   }
 }
